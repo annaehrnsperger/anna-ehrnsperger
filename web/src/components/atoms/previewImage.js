087fffa8ee5bdd132520899bc1ef6dfb2f8200ad
Anna Ehrnsperger <hallo@annaehrnsperger.de>
2021-01-22 09:55:29 +0100

    fix preview image

--- web/src/components/atoms/previewImage.js
+++ web/src/components/atoms/previewImage.js
@@ -34,6 +34,7 @@ const StyledPreviewImage = styled.figure`
   position: absolute;
   z-index: 2;
   transition: all 0.3s ease-out;
+  transition-delay: 0.05s;
   width: auto;
   top: 15%;
   display: flex;
